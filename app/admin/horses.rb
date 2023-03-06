a16bf2feb4b01aeeded0444d52ca2ab7b7cf7892
mbialowas10 <mbialowas@rrc.ca>
2023-03-06 12:36:58 -0600

    Added a daper horse to the mix

---
+++ app/admin/horses.rb
@@ -0,0 +1,25 @@
+ActiveAdmin.register Horse do
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :name, :age, :top_speed, :breed_id, :number_of_legs, :image
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :age, :top_speed, :breed_id, :number_of_legs]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+
+  form do |f|
+    f.semantic_errors
+    f.inputs
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions
+  end
+end
