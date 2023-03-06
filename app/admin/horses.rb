a2f2af46d4b8422fb2745e227dcae4f643f920e9
mbialowas10 <mbialowas@rrc.ca>
2023-03-06 10:41:22 -0600

    Daper horse image added.

---
+++ app/admin/horses.rb
@@ -0,0 +1,26 @@
+ActiveAdmin.register Horse do
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :name, :age, :top_speed, :breed_id, :number_of_legs, :image
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :age, :top_speed, :breed_id, :number_of_legs]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  form do |f|
+    f.semantic_errors
+    f.inputs
+    # let's add this piece:
+    f.inputs do
+      # f.input :image, as: :file, hint: image_tag(f.object.image)
+      f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image, size: '200x200') : ''
+    end
+    f.actions
+  end
+end
--- app/models/horse.rb
+++ app/models/horse.rb
@@ -1,3 +1,5 @@
 class Horse < ApplicationRecord
   belongs_to :breed
+  validates :name, :top_speed, :number_of_legs, :age, presence: true
+  has_one_attached :image
 end
