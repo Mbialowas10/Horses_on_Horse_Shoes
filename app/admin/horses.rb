bab28f3bc9421df10ac013670f146a18f618b600
mbialowas10 <mbialowas@rrc.ca>
2023-03-06 12:43:53 -0600

    Fixed bug with images

--- app/admin/horses.rb
+++ app/admin/horses.rb
@@ -18,7 +18,7 @@ ActiveAdmin.register Horse do
     f.semantic_errors
     f.inputs
     f.inputs do
-      f.input :image, as: :file, hint: image_tag(f.object.image)
+      f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image) : ''
     end
     f.actions
   end
