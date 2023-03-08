f42d63733410123f0faadd64d448e78fde6917de
mbialowas10 <mbialowas@rrc.ca>
2023-03-08 16:15:25 -0600

    added image to activeadmin

--- app/controllers/horses_controller.rb
+++ app/controllers/horses_controller.rb
@@ -2,10 +2,11 @@ class HorsesController < ApplicationController
   # GET /horses/:id
   def show
     @horse = Horse.find(params[:id])
+    logger.debug "Contents from horse controller #{@horse.name}"
   end
 
   # GET /horses/search?keywords=user+search+terms
   def search
-    @horses = Horse.where("name LIKE ?", "%#{params[:search_term]}%")
+    @horses = Horse.where('name LIKE ?', "%#{params[:search_term]}%")
   end
 end
